93190bf4deb8cfd626623e90a5f1ee98c0622ebc
Maximo Salinas-Miner <mes2643@g.rit.edu>
2026-09-23 08:03:25 -0400

    class

--- Game 1/test_game.py
+++ Game 1/test_game.py
@@ -11,11 +11,22 @@ pygame.display.set_caption("test game")
 
 clock = pygame.time.Clock()
 
-test_font = pygame.font.Font(None, 50)
-text_surface = test_font.render("Hello World", False, "Red")
-example_button = pygame.image.load("Game 1/graphics/buttonsprites/exbutton.png")
-wooden_floor = pygame.image.load("Game 1/graphics/scenes/woodenfloor.png")
+class Button:
+    def __init__(self, image, ):
+        self.image = pygame.image.load(image).convert_alpha()
 
+    def draw(self, x, y):
+        screen.blit(self.image, (x,y))
+
+example_button = Button("Game 1/graphics/buttonsprites/exbutton.png")
+
+        
+
+test_font = pygame.font.Font("Game 1/graphics/textfonts/HARRYP__.TTF", 50)
+text_surface = test_font.render("Hello World", False, "Red")
+wooden_floor = pygame.image.load("Game 1/graphics/scenes/woodenfloor.png")
+snail_surface = pygame.image.load("Game 1/graphics/charactersprites/snail/snail1.png")
+snail_x_pos = 600
 run = True
 while run:
 
@@ -23,9 +34,14 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    screen.blit(example_button, (200,100))
+    example_button.draw(200, 50)
     screen.blit(wooden_floor, (400,0))
     screen.blit(text_surface, (300, 50))
+    if snail_x_pos > 800:
+        snail_x_pos = 600
+    screen.blit(snail_surface, (snail_x_pos, 40))
+    snail_x_pos += 1
+
 
     pygame.display.update()
     clock.tick(60)
